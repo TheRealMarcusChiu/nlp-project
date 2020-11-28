b54b55c58657908ff9283d5c0082a307025a49ea
MahsaEalishah <mahsa.eslamialishah@utdallas.edu>
2020-11-28 17:41:41 -0600

    61.5% accuracy

--- mahsa/nlp.py
+++ mahsa/nlp.py
@@ -14,17 +14,6 @@ import torch.utils.data
 import time
 
 nlp = spacy.load("en_core_web_sm")
-entity_types = {'PERSON': 0, 'NORP': 1, 'FAC': 2, 'ORG': 3, 'GPE': 4, 'LOC': 5, 'PRODUCT': 6, 'EVENT': 7, 'WORK_OF_ART': 8,
-                'LAW': 9,'LANGUAGE': 10, 'DATE': 11, 'TIME': 12, 'PERCENT': 13, 'MONEY': 14, 'QUANTITY': 15, 'ORDINAL': 16, 
-                'CARDINAL': 17, 'UNKNOWN': 18}
-POS = {"PUNCT":0,"ADJ":1,"CCONJ":2,"NUM":3,"DET":4,"PRON":5,"ADP":6,"VERB":7,"NOUN":8,"PROPN":9,"ADV":10,"AUX":11,"SCONJ":12,
-        "INTJ":13,"other":14}
-
-prep = {"with": 0, "at": 1, "from": 2, "into": 3, "during": 4, "including": 5, "until": 6, "against": 7, "among": 8, 
-        "throughout": 9,"of": 10, "to": 11, "in": 12, "for": 13, "on": 14, "by": 15, "despite": 16, "towards": 17, "upon": 18, 
-        "about": 19, "like": 20,"through": 21, "over": 22, "before": 23, "between": 24, "after": 25, "since": 26, "without": 27,
-        "under": 28, "around": 29, "near": 30}
-
 
 
 # defining the neural network model: 3 layers fully connected
@@ -33,21 +22,18 @@ class NeuralNet(nn.Module):
         super(NeuralNet, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu1 = nn.ReLU()
-        self.Dropout1 = nn.Dropout(p=0.4)
         self.fc2 = nn.Linear(hidden_size, hidden_size2)
         self.relu2 = nn.ReLU()
-        self.Dropout2 = nn.Dropout(p=0.4)
         self.fc3 = nn.Linear(hidden_size2, num_classes)
 
     def forward(self, x):
         out = self.fc1(x)
         out = self.relu1(out)
-        out = self.Dropout1(out)
         out = self.fc2(out)
         out = self.relu2(out)
-        out = self.Dropout2(out)
         out = self.fc3(out)
         return out
+
 
 # Data loader will call CustomDataset to get the data in batches of size 64 or 32 or 16
 class CustomDataset(torch.utils.data.Dataset):
@@ -61,6 +47,7 @@ class CustomDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.X)
 
+
 # possible_relations will extract all the distinct relations that exist in data set
 def possible_relations(data):
     relation_types = {'Other'}  # set data structure
@@ -72,6 +59,7 @@ def possible_relations(data):
     relation_types = dict(zip(relation_types, range(len(relation_types))))
     return relation_types
 
+
 # data_relation_extract will return the data's relation
 def data_relation_extract(data, relation_types):
     y = np.zeros((len(data) - 1, 1))
@@ -81,163 +69,210 @@ def data_relation_extract(data, relation_types):
     return y
 
 
-def entityType(en1,en2,i,e1Type,e2Type):
-    doc = nlp(en1+','+en2)
-    for ent in doc.ents:
-        if ent.text == en1:
-            e1_type = ent.label_
-            e1Type[i, entity_types[e1_type]] = 1
-        if ent.text == en2:
-            e2_type = ent.label_
-            e2Type[i, entity_types[e2_type]] = 1
-    if sum(e1Type[i]) == 0:
-        e1Type[i, entity_types['UNKNOWN']] = 1
-    if sum(e2Type[i]) == 0:
-        e2Type[i, entity_types['UNKNOWN']] = 1
+# et_ed_sl_em will extract different features such as : e1 type, e2 type, the count of letters exists between e1 and e2,
+# sentence length, e1 word embedding, e2 word embedding.
+def et_ed_sl_em(data):
+    entity_types = {'PERSON': 0, 'NORP': 1, 'FAC': 2, 'ORG': 3, 'GPE': 4, 'LOC': 5, 'PRODUCT': 6, 'EVENT': 7,
+                    'WORK_OF_ART': 8, 'LAW': 9,
+                    'LANGUAGE': 10, 'DATE': 11, 'TIME': 12, 'PERCENT': 13, 'MONEY': 14, 'QUANTITY': 15, 'ORDINAL': 16,
+                    'CARDINAL': 17, 'UNKNOWN': 18}
+    e1S_type = np.zeros((len(data) - 1, len(entity_types)))
+    e2S_type = np.zeros((len(data) - 1, len(entity_types)))
+    e1_e2_distance = np.zeros((len(data) - 1, 1))
+    sentence_length = np.zeros((len(data) - 1, 1))
+    e1_embeding = []
+    e2_embeding = []
+    for i in range(len(data) - 1):
+        sentence_length[i] = len(data[i])
+        e1_search = re.search(r'<e1>[\W\S.,`\':$]+</e1>', data[i])
+        e1 = e1_search.group()
+        e1_location = e1_search.span()[1]
+        e11 = re.sub(r'</e1>|<e1>', "", e1).strip()
+        e2_search = re.search(r'<e2>[\W\S.,`\':$]+</e2>', data[i])
+        e2 = e2_search.group()
+        e2_location = e2_search.span()[0]
+        e22 = re.sub(r'</e2>|<e2>', "", e2).strip()
 
-def location(e1search,e2search,i,dis_matrix):
-    e1_location = e1search.span()[1]
-    e2_location = e2search.span()[1]
-    dis_matrix[i] = e2_location - e1_location
+        e1_e2_distance[i] = e2_location - e1_location
+        doc = nlp(e11 + ',' + e22)
+        for ent in doc.ents:
+            if ent.text == e11:
+                e1_type = ent.label_
+                e1S_type[i, entity_types[e1_type]] = 1
+            if ent.text == e22:
+                e2_type = ent.label_
+                e2S_type[i, entity_types[e2_type]] = 1
+        if sum(e1S_type[i]) == 0:
+            e1S_type[i, entity_types['UNKNOWN']] = 1
+        if sum(e2S_type[i]) == 0:
+            e2S_type[i, entity_types['UNKNOWN']] = 1
 
-def entity_embedding(e,mat_emb):
-    word_emb = np.zeros((96, 1))
-    word_emb = word_emb[:, 0]
-    tokens = nlp(e)
-    for token in tokens:
-        word_emb = word_emb+token.vector
-    if mat_emb is None:
-        mat_emb = word_emb
-    else:
-        mat_emb = np.vstack((mat_emb, word_emb))
-    return mat_emb
+        word_emb = np.zeros((96,))
+        tokens = nlp(e11)
+        for token in tokens:
+            word_emb = word_emb + token.vector
+        e1_embeding.append(word_emb)
 
+        word_emb_e2 = np.zeros((96,))
+        tokense2 = nlp(e22)
+        for tokene2 in tokense2:
+            word_emb_e2 = word_emb_e2 + tokene2.vector
+        e2_embeding.append(word_emb_e2)
 
-def entity_head_tag(e11,e22,e1,e2,data,i,matrix_heads): 
-    #input1 = re.sub(r'</e2>|<e2>|</e1>|<e1>', "", data)
-    #input1 = input1.replace("  "," ")
-    indx = 0
-    e11 = e11.replace("-","")
-    e11 = e11.replace("_","")
-    e11 = e11.replace(" ","")
-    e22 = e22.replace("-","")
-    e22 = e22.replace("_","")
-    e22 = e22.replace(" ","")
-    input1=data.replace(e2,' '+e22+' ')
-    input1=input1.replace(e1,' '+e11+' ')
-    input1 = input1.replace("  "," ")
-    statement =nlp(input1)
-    for token in statement:
-        if '<e1>'+token.text+'</e1>' == e1 :
-            if POS.get(token.head.pos_) is not None:
-                matrix_heads[i , POS[token.head.pos_]]=1
-            else:
-                matrix_heads[i , POS["other"]]=1
-            indx+=1
-        elif '<e2>'+token.text+'</e2>' == e2 :
-            if POS.get(token.head.pos_) is not None:
-                matrix_heads[i , POS[token.head.pos_]]=1
-            else:
-                matrix_heads[i,POS["other"]]=1
-            indx+=1
-        if indx==2:
-            break
+    return e1S_type, e2S_type, e1_e2_distance, sentence_length, np.array(e1_embeding), np.array(e2_embeding)
 
-
-def sdp(e11,e22,e1,e2,data,i,sdp_matrix):
-    data1 = data.split("\n")[0]
-    e11 = e11.replace("-","")
-    e11 = e11.replace("_","")
-    e11 = e11.lower()
-    e22 = e22.replace("-","")
-    e22 = e22.replace("_","")
-    e22 = e22.lower()
-    
-    input1=data1.replace(e2,' '+e22+' ')
-    input1=input1.replace(e1,' '+e11+' ')
-    input1 = input1.replace("  "," ")
-    doc = nlp(input1)
-    edges = []
-    for token in doc:
-        for child in token.children:
-            edges.append(('{0}'.format(token.lower_),
-                        '{0}'.format(child.lower_)))
-
-    graph = nx.Graph(edges)
-    try :        
-        len_sdp = nx.shortest_path_length(graph, source=e11, target=e22)
-    except : 
-        len_sdp = -1
-    sdp_matrix[i] = len_sdp
-    #print(nx.shortest_path(graph, source=entity1, target=entity2))
 
 # will extract the propesitions between e1 and e2 and will extract the tokens between
 #  e1,e2 entities and represent them using word embeding
-def prep_emb(data,i,POS_beetween_e1e2,sentence_prep,e1_e2_embeding):    
-    data_between_entities = re.search(
-        r'</e1>(.*)<e2>', data).group()
-    data_between_entities2 = re.sub(
-        r'</e1>\s|\s<e2>', "", data_between_entities)
-    doc = nlp(data_between_entities2)
-    word_emb_e1_e2 = np.zeros((96, 1))
-    word_emb_e1_e2 = word_emb_e1_e2[:, 0]
-    for token in doc:
-        if token.tag_ == "IN":
-            if prep.get(token.text) is not None:
-                sentence_prep[i, prep[token.text]]+=1
-        if POS.get(token.pos_) is None:
-            POS_beetween_e1e2[i,POS["other"]]+=1
-        else:
-            POS_beetween_e1e2[i,POS[token.pos_]]+=1
+def prep_emb(data):
+    prep = {"with": 0, "at": 1, "from": 2, "into": 3, "during": 4, "including": 5, "until": 6, "against": 7, "among": 8,
+            "throughout": 9,
+            "of": 10, "to": 11, "in": 12, "for": 13, "on": 14, "by": 15, "despite": 16, "towards": 17, "upon": 18,
+            "about": 19, "like": 20,
+            "through": 21, "over": 22, "before": 23, "between": 24, "after": 25, "since": 26, "without": 27,
+            "under": 28, "around": 29, "near": 30}
+    POS = {"PUNCT": 0, "ADJ": 1, "CCONJ": 2, "NUM": 3, "DET": 4, "PRON": 5, "ADP": 6, "VERB": 7, "NOUN": 8, "PROPN": 9,
+           "ADV": 10, "AUX": 11, "other": 12}
+    POS_beetween_e1e2 = np.zeros((len(data) - 1, len(POS)))
+    sentence_prep = np.zeros((len(data) - 1, len(prep)))
+    e1_e2_embeding = []
+    for i in range(len(data) - 1):
+        data_between_entities = re.search('</e1>(.*)<e2>', data[i]).group(1).strip()
+        doc = nlp(data_between_entities)
+        word_emb_e1_e2 = np.zeros((96,))
+        for token in doc:
+            if token.tag_ == "IN":
+                if prep.get(token.text) is not None:
+                    sentence_prep[i, prep[token.text]] = 1
 
-        word_emb_e1_e2 = word_emb_e1_e2+token.vector
-    if e1_e2_embeding is None:
-        e1_e2_embeding = word_emb_e1_e2
-    else:
-        e1_e2_embeding = np.vstack((e1_e2_embeding, word_emb_e1_e2))
-    return e1_e2_embeding
+            if POS.get(token.pos_) is None:
+                POS_beetween_e1e2[i, POS["other"]] = 1
+            else:
+                POS_beetween_e1e2[i, POS[token.pos_]] = 1
 
-def emb_after_before(data,before_e1_embeding,aft_e2_embeding):
-    data = data.split("\n")[0]
-    before_e1 = re.search(r'[\W\S.,`\':$<>/]+<e1>', data).group()
-    data_before_split_e1 = before_e1.split(" ")
-    try:
-        data_before_e1 = data_before_split_e1[-2]
-    except:
-        data_before_e1 = 'start'
-    try:
-        data_before_e1 = data_before_e1.split("\t")[1]
-    except:
-        data_before_e1 = data_before_e1
-    doc = nlp(data_before_e1)
-    word_emb_bef_e1 = np.zeros((96, 1))
-    word_emb_bef_e1 = word_emb_bef_e1[:, 0]
-    for token in doc:
-        word_emb_bef_e1 = word_emb_bef_e1+token.vector
-    if before_e1_embeding is None:
-        before_e1_embeding = word_emb_bef_e1
-    else:
-        before_e1_embeding = np.vstack((before_e1_embeding, word_emb_bef_e1))
+            word_emb_e1_e2 = word_emb_e1_e2 + token.vector
+        e1_e2_embeding.append(word_emb_e1_e2)
+
+    return sentence_prep, np.array(e1_e2_embeding), POS_beetween_e1e2
+
+def entity_head_tag(data):
+    POS = {"PUNCT": 0, "ADJ": 1, "CCONJ": 2, "NUM": 3, "DET": 4, "PRON": 5, "ADP": 6, "VERB": 7, "NOUN": 8, "PROPN": 9,
+           "ADV": 10, "AUX": 11, "other": 12}
+    matrix_heads = np.zeros((len(data)-1, len(POS))) 
+    for i in range(len(data)-1):
+        e1_search = re.search(r'<e1>[\W\S.,`\':$]+</e1>', data[i])
+        e1 = e1_search.group()
+        e11 = re.sub(r'</e1>|<e1>', "", e1)
+        e11 = e11.replace(" ", "") 
+        e11 = e11.replace("-","")
+        e11 = e11.replace("_","")
+        e11 = e11.replace(" ","")
+        e2_search = re.search(r'<e2>[\W\S.,`\':$]+</e2>', data[i])
+        e2 = e2_search.group()
+        e22 = re.sub(r'</e2>|<e2>', "", e2)
+        e22 = e22.replace(" ", "")
+        e22 = e22.replace("-","")
+        e22 = e22.replace("_","")
+        e22 = e22.replace(" ","")
+        indx = 0
+        input1=data[i].replace(e2,' '+e22+' ')
+        input1=input1.replace(e1,' '+e11+' ')
+        input1 = input1.replace("  "," ")
+        statement =nlp(input1)
+        for token in statement:
+            if '<e1>'+token.text+'</e1>' == e1 :
+                if POS.get(token.head.pos_) is not None:
+                    matrix_heads[i , POS[token.head.pos_]]=1
+                else:
+                    matrix_heads[i , POS["other"]]=1
+                indx+=1
+            elif '<e2>'+token.text+'</e2>' == e2 :
+                if POS.get(token.head.pos_) is not None:
+                    matrix_heads[i , POS[token.head.pos_]]=1
+                else:
+                    matrix_heads[i,POS["other"]]=1
+                indx+=1
+            if indx==2:
+                break
+    return matrix_heads
 
 
-    after_e2 = re.search(r'</e2>[\W\S.,`\':$<>/]+', data).group()
-    data_after_split_e2 = after_e2.split(" ")
-    try:
-        data_after_e2 = data_after_split_e2[1]
-    except:
-        data_after_e2 = 'end'
+def sdp(data):
+    sdp_matrix = np.zeros((len(data)-1, 1))
+    for i in range(len(data)-1):
+        data1 = data[i].split("\n")[0]
+        e1_search = re.search(r'<e1>[\W\S.,`\':$]+</e1>', data[i])
+        e1 = e1_search.group()
+        e11 = re.sub(r'</e1>|<e1>', "", e1)
+        e11 = e11.replace(" ", "")
+        e11 = e11.replace("-","")
+        e11 = e11.replace("_","")
+        e11 = e11.lower()         
+        e2_search = re.search(r'<e2>[\W\S.,`\':$]+</e2>', data[i])
+        e2 = e2_search.group()
+        e22 = re.sub(r'</e2>|<e2>', "", e2)
+        e22 = e22.replace(" ", "")
+        e22 = e22.replace("-","")
+        e22 = e22.replace("_","")
+        e22 = e22.lower()    
+        input1=data1.replace(e2,' '+e22+' ')
+        input1=input1.replace(e1,' '+e11+' ')
+        input1 = input1.replace("  "," ")
+        doc = nlp(input1)
+        edges = []
+        for token in doc:
+            for child in token.children:
+                edges.append(('{0}'.format(token.lower_),
+                            '{0}'.format(child.lower_)))
 
-    doc = nlp(data_after_e2)
-    word_emb_aft_e2 = np.zeros((96, 1))
-    word_emb_aft_e2 = word_emb_aft_e2[:, 0]
-    for token in doc:
-        word_emb_aft_e2 = word_emb_aft_e2+token.vector
-    if aft_e2_embeding is None:
-        aft_e2_embeding = word_emb_aft_e2
-    else:
-        aft_e2_embeding = np.vstack((aft_e2_embeding, word_emb_aft_e2))
+        graph = nx.Graph(edges)
+        try :        
+            len_sdp = nx.shortest_path_length(graph, source=e11, target=e22)
+        except : 
+            len_sdp = -1
+        sdp_matrix[i] = len_sdp
+        #print(nx.shortest_path(graph, source=entity1, target=entity2))
+    return sdp_matrix
 
-    return before_e1_embeding,aft_e2_embeding
+def emb_after_before(data):
+    before_e1_embeding = []
+    aft_e2_embeding = []
+    for i in range(len(data)-1):
+        data1 = data[i].split("\n")[0]
+        before_e1 = re.search(r'[\W\S.,`\':$<>/]+<e1>', data1).group()
+        data_before_split_e1 = before_e1.split(" ")
+        try:
+            data_before_e1 = data_before_split_e1[-2]
+        except:
+            data_before_e1 = 'start'
+        try:
+            data_before_e1 = data_before_e1.split("\t")[1]
+        except:
+            data_before_e1 = data_before_e1
+        doc = nlp(data_before_e1)
+        word_emb_bef_e1 = np.zeros((96, 1))
+        word_emb_bef_e1 = word_emb_bef_e1[:, 0]
+        for token in doc:
+            word_emb_bef_e1 = word_emb_bef_e1+token.vector
+        before_e1_embeding.append(word_emb_bef_e1)
+
+
+        after_e2 = re.search(r'</e2>[\W\S.,`\':$<>/]+', data1).group()
+        data_after_split_e2 = after_e2.split(" ")
+        try:
+            data_after_e2 = data_after_split_e2[1]
+        except:
+            data_after_e2 = 'end'
+
+        doc = nlp(data_after_e2)
+        word_emb_aft_e2 = np.zeros((96, 1))
+        word_emb_aft_e2 = word_emb_aft_e2[:, 0]
+        for token in doc:
+            word_emb_aft_e2 = word_emb_aft_e2+token.vector
+        aft_e2_embeding.append(word_emb_aft_e2)
+
+    return np.array(before_e1_embeding),np.array(aft_e2_embeding)
+
 
 
 def get_relation_list(file_name):
@@ -252,47 +287,19 @@ def get_features(file_name, relations_list):
     file_ = open(file_name)
     corpus = file_.read()
     file_.close()
-    data = corpus.split("\n\n\n")
+    splited_data = corpus.split("\n\n\n")
 
-    ys = data_relation_extract(data, relations_list)
+    ys = data_relation_extract(splited_data, relations_list)
+    prepositions, word_emb_e1_e2, POS_beetween_e1e2 = prep_emb(splited_data)
+    e1Type, e2Type, e1e2distance, sentence_length, e1_emb, e2_emb = et_ed_sl_em(splited_data)
+    POS_head = entity_head_tag(splited_data)
+    sdp_e1_e2 = sdp(splited_data)
+    Wbef_e1,Waft_e2 = emb_after_before(splited_data)
 
-    sentence_length = np.zeros((len(data)-1, 1))
-    e1S_type = np.zeros((len(data)-1, len(entity_types)))
-    e2S_type = np.zeros((len(data)-1, len(entity_types)))
-    e1_e2_distance = np.zeros((len(data)-1, 1))
-    e1_embeding = None
-    e2_embeding = None
-    POS_head = np.zeros((len(data)-1, len(POS)))
-    sdp_e1_e2 = np.zeros((len(data)-1, 1))
-    POS_beetween_e1e2 = np.zeros((len(data)-1, len(POS)))
-    sentence_prep = np.zeros((len(data)-1, len(prep)))
-    e1_e2_embeding = None
-    Waft_e2 = None
-    Wbef_e1= None
+    features = np.concatenate((e1Type, e2Type,e1e2distance, prepositions, e1_emb,
+                e2_emb,word_emb_e1_e2,POS_beetween_e1e2), axis=1)
 
-    for i in range(len(data)-1):
-        sentence_length[i] = len(data[i].split("\n")[0])
-        e1_search = re.search(r'<e1>[\W\S.,`\':$]+</e1>', data[i])
-        e1 = e1_search.group()
-        e11 = re.sub(r'</e1>|<e1>', "", e1)
-        e11 = e11.replace(" ", "") 
-        e2_search = re.search(r'<e2>[\W\S.,`\':$]+</e2>', data[i])
-        e2 = e2_search.group()
-        e22 = re.sub(r'</e2>|<e2>', "", e2)
-        e22 = e22.replace(" ", "")
-        entityType(e11,e22,i,e1S_type,e2S_type)
-        location(e1_search,e2_search,i,e1_e2_distance)
-        e1_embeding = entity_embedding(e11,e1_embeding) 
-        e2_embeding = entity_embedding(e22,e2_embeding)
-        entity_head_tag(e11,e22,e1,e2,data[i],i,POS_head)
-        sdp(e11,e22,e1,e2,data[i],i,sdp_e1_e2)
-        e1_e2_embeding = prep_emb(data[i],i,POS_beetween_e1e2,sentence_prep,e1_e2_embeding)
-        Wbef_e1,Waft_e2 = emb_after_before(data[i],Wbef_e1,Waft_e2)
-
-    features = np.concatenate((e1S_type, e2S_type, sentence_prep, e1_embeding,
-                        e2_embeding,e1_e2_embeding,POS_beetween_e1e2,POS_head,sdp_e1_e2,Wbef_e1,Waft_e2), axis=1)
-
-    return features, ys
+    return features,ys
 
 
 def build_and_train_model(X_features_train, Y_train, num_classes):
@@ -300,9 +307,9 @@ def build_and_train_model(X_features_train, Y_train, num_classes):
 
     # Hyper parameters
     hidden_size = 500
-    hidden_size2 = 300
+    hidden_size2 = 250
     num_epochs = 80
-    batch_size = 64
+    batch_size = 16
     learning_rate = 0.001
 
     train_loader = torch.utils.data.DataLoader(  # Reading the training set features in batches
@@ -365,6 +372,7 @@ def compute_cm_without_direction(cm):
     cols_odd = np.hstack((cols_odd, np.zeros(shape=(len(cols_even[0]), 1))))
     return cols_even + cols_odd
 
+
 def predict_and_compute_confusion_matrix(model, X_features_test, Y_test, relations_list):
     # compute confusion matrix
     cm = np.zeros((len(relations_list), len(relations_list)))
@@ -421,6 +429,4 @@ print('prediction time for {} samples : {}'.format(len(Y_test), end - start))
 print('prediction time for 1 sample : {}'.format((end - start) / len(Y_test)))
 
 compute_scores(cm)
-
-
 
