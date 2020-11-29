5b9afd22ee7eb9eb424fe0386b407b0c7703471f
Marcus Chiu <marcuschiu@marcuss-mbp-2.lan>
2020-11-29 16:06:52 -0600

    cleaned up code

--- marcus-code/experimental/other.py
+++
@@ -1,5 +0,0 @@
-from gensim.models import KeyedVectors
-
-model = KeyedVectors.load_word2vec_format('data/GoogleNews-vectors-negative300.bin', binary=True)
-sim = model.most_similar('robots')
-print(sim)
